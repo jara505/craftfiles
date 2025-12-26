d77a3280ee63996f47a1236279d989d9d7a7745c
jara505 <juanignaciojara505@gmail.com>
2025-12-25 19:11:57 -0600

    fix: make --version dynamic from package.json

--- bin/craftfiles.js
+++ bin/craftfiles.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import { readFileSync } from 'fs';
 import { initCommand } from '../src/index.js';
+
+const packageJson = JSON.parse(readFileSync(new URL('../package.json', import.meta.url)));
+const version = packageJson.version;
 
 program
   .name('craftfiles')
   .description('CLI to initialize and generate config files for JS/TS projects')
-  .version('1.0.0')
+  .version(version)
   .option('--creator', 'Show creator info');
 
 program
